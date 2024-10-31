d7c2ef444a19239d2f1ae8a41bea87f232fada6a
erizomovil <jg.arcalagonzalez@gmail.com>
2024-10-31 20:32:53 +0000

    Working in navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,32 +1,22 @@
 import React from "react";
 import { BsSearch } from "react-icons/bs";
+import { FaSliders } from "react-icons/fa6";
 import "./Navbar.css";
 
 const Navbar: React.FC = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <nav className="navbar navbar-light bg-light">
       <div className="container-fluid">
-        <div className="d-flex justify-content-between w-100">
-          <div className="navbar-brand d-flex align-items-center">
-            <img
-              src="logo.png"
-              alt="Logo"
-              width="30"
-              height="30"
-              className="me-2"
-            />
-            <span>Faire Plats</span>
-          </div>
-          <div className="d-flex">
+        <div className="navbar-mobile">
+          <div className="navbar-input">
+            <BsSearch className="icon" />
             <input
               type="search"
-              className="form-control me-2"
+              className="search-input"
               placeholder="buscar"
               aria-label="Buscar"
             />
-            <div>
-              <BsSearch />
-            </div>
+            <FaSliders className="icon" />{" "}
           </div>
         </div>
       </div>
