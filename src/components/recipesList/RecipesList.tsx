b58d7842ead3ca67b73349abb5b65c566aab443c
erizomovil <jg.arcalagonzalez@gmail.com>
2024-11-11 20:33:24 +0000

    Arreglado el Grid

--- src/components/recipesList/RecipesList.tsx
+++ src/components/recipesList/RecipesList.tsx
@@ -10,6 +10,9 @@ function RecipesLists() {
         <Card title={"Omelete"} dificulty={2} time={12} />
         <Card title={"Hamburger"} dificulty={2} time={10} />
         <Card title={"Spagetty a la boloñesa"} dificulty={2} time={15} />
+        <Card title={"Saltburry Steak"} dificulty={3} time={60} />
+        <Card title={"Rattatui"} dificulty={4} time={90} />
+        <Card title={"Lasagna titu mama"} dificulty={5} time={120} />
       </div>
     </>
   );
