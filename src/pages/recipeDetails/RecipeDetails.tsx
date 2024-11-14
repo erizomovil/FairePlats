85ae5f99274bedcc19c3dfe9f0cc83d7182ddecd
erizomovil <jg.arcalagonzalez@gmail.com>
2024-11-14 14:27:59 +0000

    Revisar recipeDetails

--- src/pages/recipeDetails/RecipeDetails.tsx
+++ src/pages/recipeDetails/RecipeDetails.tsx
@@ -1,11 +1,28 @@
+import StaticStars from "../../components/stars/StaticStars";
 import "./RecipeDetails.css"
+import { FaArrowLeft, FaStar } from "react-icons/fa";
 
 function RecipeDetails() {
-    
     return (
         <>
-            <h1>Hola, aqui va la receta</h1>
+            <div className="recipe-details">
+                <button className="back-button">
+                    <FaArrowLeft />
+                </button>
+                <div className="image-container">
+                    <img 
+                        src="" 
+                        alt="Imagen de la receta" 
+                        className="recipe-image" 
+                    />
+                </div>
+                <p className="recipe-title">Fried Egg</p>
+                <div className="rating-container">
+                    <StaticStars rating={1} />
+                </div>
+            </div>
         </>
-    )
+    );
 }
-export default RecipeDetails
+
+export default RecipeDetails;
